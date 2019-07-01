9f134ce876e409b51f55d2acdf7d4a47398ebfea
wsl007 <13402787936@139.com>
2019-07-01 21:39:26 +0800

    使用databinding

--- app/src/main/java/com/wsl/viewbykt/MainActivity.kt
+++ app/src/main/java/com/wsl/viewbykt/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.wsl.viewbykt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.wsl.viewbykt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
